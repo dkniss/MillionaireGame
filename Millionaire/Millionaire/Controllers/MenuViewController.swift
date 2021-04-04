5e44d887884e6518eb19ad144569e32c15adc12c
Daniil <danielkniss@mail.ru>
2021-04-04 13:45:11 +0500

    Added MenuViewController

---
+++ Millionaire/Millionaire/Controllers/MenuViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MenuViewController.swift
+//  Millionaire
+//
+//  Created by Daniil Kniss on 04.04.2021.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "startScreenBackground")!)
+    }
+
+}
